97b29ebf4b8ee9c75700a731ee11d61fee5ddba3
Durward Craig Jr <Durward.CraigJr@outlook.com>
2016-04-13 12:35:48 +0300

    Improvements

---
+++ The Adventure/Assets/Scripts/ImporterSettings.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TexturePostProcessor : AssetPostprocessor
+{
+    bool runscript = false;
+
+    void OnPostprocessTexture(Texture texture)
+    {
+        if (runscript)
+        {
+            TextureImporter importer = assetImporter as TextureImporter;
+            importer.textureType = TextureImporterType.Image;
+            importer.anisoLevel = 16;
+            importer.filterMode = FilterMode.Bilinear;
+
+            Object asset = AssetDatabase.LoadAssetAtPath(importer.assetPath, typeof(Texture));
+            if (asset)
+            {
+                EditorUtility.SetDirty(asset);
+            }
+            else
+            {
+                texture.anisoLevel = 16;
+                texture.filterMode = FilterMode.Bilinear;
+            }
+        }
+    }
+}
